8cd803a859423d6f897b51a422901ca5a6a26397
José Martín-Cobos Ortega <josemartin_cobos@yahoo.es>
2013-11-01 03:25:45 +0100

    modifying the BoardingCards classes

--- TripSort/src/com/jmc/tripsort/bean/BusBoardingCard.java
+++ TripSort/src/com/jmc/tripsort/bean/BusBoardingCard.java
@@ -2,9 +2,25 @@ package com.jmc.tripsort.bean;
 
 public class BusBoardingCard extends BoardingCard {
 
+	private String number;
+
 	public BusBoardingCard(String from, String to) {
 		super(from, to);
-		// TODO Auto-generated constructor stub
+		super.setType("BUS");
 	}
 
+	public BusBoardingCard(String from, String to, String number) {
+		super(from, to);
+		super.setType("BUS");
+		this.setNumber(number);
+		
+	}
+	
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
 }
--- TripSort/src/com/jmc/tripsort/bean/CruiserBoardingCard.java
+++ TripSort/src/com/jmc/tripsort/bean/CruiserBoardingCard.java
@@ -1,10 +1,37 @@
 package com.jmc.tripsort.bean;
 
 public class CruiserBoardingCard extends BoardingCard {
+	
+	private String dock;
+	private String cabinNumber;		
 
-	public CruiserBoardingCard(String from, String to) {
+	
+	public CruiserBoardingCard(String from, String to){
 		super(from, to);
+		super.setType("Cruiser");
+	}
+	
+	public CruiserBoardingCard(String from, String to, String number, String cabinNumber, String dock) {
+		super(from, to);
+		super.setType("Cruiser");
+		this.setCabinNumber(cabinNumber);
+		this.setDock(dock);
 		
 	}
 
+	public String getDock() {
+		return dock;
+	}
+
+	public String getCabinNumber() {
+		return cabinNumber;
+	}
+
+	public void setDock(String dock) {
+		this.dock = dock;
+	}
+
+	public void setCabinNumber(String cabinNumber) {
+		this.cabinNumber = cabinNumber;
+	}
 }
