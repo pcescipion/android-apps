b706d7acdd4075ecc2d721a0b69f64305681f1d8
José Martín-Cobos Ortega <josemartin_cobos@yahoo.es>
2013-11-01 01:32:18 +0100

    new changes

--- TripSort/src/com/jmc/tripsort/bean/CruiserBoardingCard.java
+++ TripSort/src/com/jmc/tripsort/bean/CruiserBoardingCard.java
@@ -4,7 +4,7 @@ public class CruiserBoardingCard extends BoardingCard {
 
 	public CruiserBoardingCard(String from, String to) {
 		super(from, to);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
